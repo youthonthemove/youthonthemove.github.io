ac97ed69f21222ab873f95de210fc40f706866cd
inforjeuneswaterloo <129519866+inforjeuneswaterloo@users.noreply.github.com>
2025-04-10 18:26:54 +0200

    deploy latest build

--- _site/assets/js/menu-button.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuButton = document.querySelector('.menu-button');
-    const navDrawer = document.querySelector('.nav-drawer');
-  
-    menuButton.addEventListener('click', function() {
-      navDrawer.classList.toggle('open');
-    });
-  });
